90cb993b7afe469936401a55acf875580b62b981
abhishekpaturkar <godkillop03@gmail.com>
2024-07-21 20:49:39 +0530

    0169 Majority Element

---
+++ Array/MajorityElement.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int count = 0;
+        int candidate = nums[0];
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (count == 0)
+            {
+                candidate = nums[i];
+                count++;
+            }
+            else if (nums[i] == candidate)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return candidate;
+    }
+};

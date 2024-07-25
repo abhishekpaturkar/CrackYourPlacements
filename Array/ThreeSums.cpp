dbcd61787e2ad1dc34df273e58cfa6af7bf2144e
abhishekpaturkar <godkillop03@gmail.com>
2024-07-25 21:23:24 +0530

    0015 3 sum

---
+++ Array/ThreeSums.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        int target = 0;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        set<vector<int>> s;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target)
+                {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                }
+                else if (sum < target)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+        }
+        // inserting elements in set to result
+        for (auto triplet : s)
+        {
+            result.push_back(triplet);
+        }
+        return result;
+    }
+};
